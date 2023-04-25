7e5a1df418b274fc171b78de86958898e6cd9c3e
Pongsakorn Weraburudpongsatorn <ping_pongsakorn@hotmail.com>
2023-04-25 14:08:16 +0700

    +

---
+++ project/lib/HomeBox.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeBox extends StatelessWidget {
+  String title = "";
+  double amount = 0.0;
+  Color color = Colors.black;
+  double size = 0.0;
+
+  HomeBox(this.title, this.amount, this.color, this.size);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(15)),
+      height: size,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+              child: Text(
+            amount.toString(),
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.right,
+          ))
+        ],
+      ),
+    );
+  }
+}
