7148da2be6da413136725bde19f8493a85fe3301
Thunder <121246118+KaminariSora@users.noreply.github.com>
2023-05-17 23:45:28 +0700

    Create RowLine.dart

---
+++ project/lib/RowLine.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import 'countIcon.dart';
+
+class RowLine extends StatelessWidget {
+  final String type1,type2;
+  const RowLine({Key? key,required this.type1,required this.type2}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(40, 6, 40, 0),
+          child: Container(
+            alignment: Alignment.center,
+            height: 34,
+            //color: Colors.amber,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(40, 60, 40, 0),
+          child: Container(
+            alignment: Alignment.center,
+            height: 34,
+            //color: Colors.amber,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Text(
+                    type1,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      height: 1.5,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                ),
+                Expanded(child: CountIcon()),
+                SizedBox(
+                  width: 24,
+                ),
+                Expanded(child: CountIcon()),
+                SizedBox(
+                   width: 24,
+                 ),
+                Expanded(
+                  child: Text(
+                    type2,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      height: 1.5,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        // Padding(
+        //   padding: EdgeInsets.fromLTRB(40, 114, 40, 0),
+        //   child: Container(
+        //     alignment: Alignment.center,
+        //     height: 34,
+        //     color: Colors.amber,
+        //     child: Row(
+        //       crossAxisAlignment: CrossAxisAlignment.center,
+        //     ),
+        //   ),
+        // ),
+        // Padding(
+        //   padding: EdgeInsets.fromLTRB(40, 168, 40, 0),
+        //   child: Container(
+        //     alignment: Alignment.center,
+        //     height: 34,
+        //     color: Colors.amber,
+        //     child: Row(
+        //       crossAxisAlignment: CrossAxisAlignment.center,
+        //     ),
+        //   ),
+        // )
+      ]),
+    );
+  }
+}
