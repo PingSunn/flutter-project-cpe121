1b5d0b5ed656d4c275e3b16e2a14fb6a968aaafe
NaphatNu <naphat.nuchitprasitchai@m.swu.ac.th>
2023-05-21 19:17:06 +0700

    fi

--- project/lib/WeightControl.dart
+++ project/lib/WeightControl.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mybasicapp/BigWidget1.dart';
+import 'package:mybasicapp/SmallWidget.dart';
 
 
 class WeightControl extends StatelessWidget {
@@ -13,54 +14,106 @@ class WeightControl extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+       home: MyHomePage(title: 'Flutter Demo Home Page',),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
 
-  final String title;
+  BigWidget1 Bwg = BigWidget1();
+  SmallWidget Swg1 = SmallWidget(total: 'รวม(กรัม)',data: 'ผลไม้',unit1: 'ผล',unit2: 'ชิ้น',);
+  SmallWidget Swg2 = SmallWidget(total: 'รวม(มล.)',data: 'นม',unit1: 'กล่อง',unit2: 'แก้ว',);
+  //final DataCount Bwgdata = DataCount();
+  MyHomePage({Key? key,required String title,}) : super(key: key);
+  
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("My Basic App")),
-        body: Padding(
-          padding: EdgeInsets.all(10.0),
-          child:  Column(children: [
-            //MyBox(),
-            BigWidget1(),
-            SizedBox(height: 10,),
-            Padding(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: Center(
-                      child: ElevatedButton(onPressed: (){
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(
+                    "อาหารจานสุขภาพ",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Twist',
+                        color: Colors.black),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(
+                    "Healthy Food",
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Bwg,
+                SizedBox(
+                  height: 10,
+                ),
+                Swg1,
+                SizedBox(
+                  height: 10,
+                ),
+                Swg2,
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        
+                        print(Bwg.getRLdataMeat());
+                        print(Bwg.getRLdataRice());
+                        print(Bwg.getRLdataVeget());
+                        print(Swg1.getRLSmallWidget());
+
                         print("Confirm");
-                      }, child: Row(
+                      },
+                      child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Confirm"),
+                          Text(
+                            "Confirm",
+                            style:
+                                TextStyle(fontFamily: 'Twist', fontSize: 32),
+                          ),
                         ],
                       ),
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        foregroundColor: Colors.black,
-                        padding: EdgeInsets.symmetric(horizontal: 100),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                        textStyle: TextStyle(fontSize: 30,fontFamily: 'Twist')
-                      ),
-                      ),
+                          backgroundColor: Colors.green,
+                          foregroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(horizontal: 100),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          textStyle:
+                              TextStyle(fontSize: 30, fontFamily: 'Twist')),
                     ),
-                  )
-          ],)
-        ),
-      );
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
