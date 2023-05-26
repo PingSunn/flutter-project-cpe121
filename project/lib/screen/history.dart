738d8945e070470214e3eb2173f99b011b953ed9
Thunder <nonthacha.h@gmail.com>
2023-05-26 11:55:12 +0700

    Merge branch 'KaminariSora-patch-1' of https://github.com/PingSunn/flutter-project-cpe121 into KaminariSora-patch-1

--- project/lib/screen/history.dart
+++ project/lib/screen/history.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:project/Diet.dart';
 import 'package:project/models/transections.dart';
 import 'package:project/provider/transection_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 
 class History extends StatelessWidget {
-  const History({super.key});
+  const History({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,16 +20,22 @@ class History extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const Profile(title: "ประวัติ"),
+        home: Profile(
+          title: "ประวัติ",
+          navigateBack: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
     );
   }
 }
 
 class Profile extends StatefulWidget {
-  const Profile({super.key, required this.title});
+  const Profile({Key? key, required this.title, required this.navigateBack});
 
   final String title;
+  final VoidCallback navigateBack;
 
   @override
   State<Profile> createState() => _ProfileState();
@@ -39,115 +44,83 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     Provider.of<TransectionProvider>(context, listen: false).initData();
   }
 
   @override
   Widget build(BuildContext context) {
-    bool order = false;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            widget.title,
-            style: TextStyle(
-              fontSize: 30,
-              fontFamily: 'Twist',
-              shadows: [
-                BoxShadow(color: Colors.black, offset: Offset(5, 5), spreadRadius: 10, blurRadius: 10)
-              ],
-            ),
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: 'Twist',
+            shadows: [
+              BoxShadow(color: Colors.black, offset: Offset(5, 5), spreadRadius: 10, blurRadius: 10),
+            ],
           ),
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(begin: Alignment.centerLeft, end: Alignment.centerRight, colors: [
-                Color(0xffeeaeca),
-                Color(0xff94bbe9)
-              ]),
-            ),
-          ),
-          leading: IconButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => Weight(
-                              title: '',
-                            )));
-              },
-              icon: Icon(Icons.arrow_back)),
-          // actions: [
-          //   IconButton(
-          //       onPressed: () {
-          //         setState(() {
-          //           order = !order;
-          //         });
-          //       },
-          //       icon: Icon(Icons.restore))
-          // ],
         ),
-        body: Consumer(
-          builder: (context, TransectionProvider provider, Widget? child) {
-            var count = provider.transection.length; //นับจำนวนข้อมูล
-            if (count <= 0) {
-              return Center(
-                child: Text(
-                  "ไม่พบข้อมูล",
-                  style: TextStyle(fontSize: 35),
-                ),
-              );
-            } else {
-              return ListView.builder(
-                reverse: false,
-                itemCount: count,
-                itemBuilder: (context, int index) {
-                  transections data = provider.transection[index];
-                  return Card(
-                    elevation: 8,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: AssetImage('assets/images/clock.png'),
-                        radius: 30,
-                      ),
-                      title: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            DateFormat("dd/MM/yyyy hh:mm:ss aaa ").format(data.date),
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          Text("เนื้อ = ${data.dataMeat.toString()} กรัม"),
-                          Text("ข้าว = ${data.dataRice.toString()} กรัม"),
-                          Text("ผัก = ${data.dataVeget.toString()} กรัม"),
-                          Text("ผลไม้ = ${data.dataFruit.toString()} กรัม"),
-                          Text("นม = ${data.dataMilk.toString()} มิลลิลิตร"),
-                        ],
-                      ),
-                      dense: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.centerLeft, end: Alignment.centerRight, colors: [
+              Color(0xffeeaeca),
+              Color(0xff94bbe9),
+            ]),
+          ),
+        ),
+        leading: IconButton(
+          onPressed: widget.navigateBack,
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Consumer(
+        builder: (context, TransectionProvider provider, Widget? child) {
+          var count = provider.transection.length; //นับจำนวนข้อมูล
+          if (count <= 0) {
+            return Center(
+              child: Text(
+                "ไม่พบข้อมูล",
+                style: TextStyle(fontSize: 35),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              reverse: false,
+              itemCount: count,
+              itemBuilder: (context, int index) {
+                transections data = provider.transection[index];
+                return Card(
+                  elevation: 8,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/clock.png'),
+                      radius: 30,
                     ),
-                  );
-                },
-                // itemBuilder: (context, int index) {
-                //   transections data = provider.transection[index];
-                //   return Card(
-                //     margin: EdgeInsets.all(5),//ระยะห่าง card
-                //     elevation: 5,//เงา
-                //     child: ListTile(
-                //       leading: CircleAvatar(
-                //           radius: 30,
-                //           child: FittedBox(
-                //             child: Text(""),
-                //           )),
-                //       title: Text(index.toString()),
-                //       subtitle: Text(""),
-                //     ),
-                //   );
-                // }
-              );
-            }
-          },
-        ));
+                    title: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          DateFormat("dd/MM/yyyy hh:mm:ss aaa ").format(data.date),
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text("เนื้อ = ${data.dataMeat.toString()} กรัม"),
+                        Text("ข้าว = ${data.dataRice.toString()} กรัม"),
+                        Text("ผัก = ${data.dataVeget.toString()} กรัม"),
+                        Text("ผลไม้ = ${data.dataFruit.toString()} กรัม"),
+                        Text("นม = ${data.dataMilk.toString()} มิลลิลิตร"),
+                      ],
+                    ),
+                    dense: true,
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
   }
 }
