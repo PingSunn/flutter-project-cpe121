63a82289e988f7225c22c5be73b80cd2e9483c09
Pongsakorn Weraburudpongsatorn <ping_pongsakorn@hotmail.com>
2023-05-26 19:55:53 +0700

    boxShadow

--- project/lib/HomeBox.dart
+++ project/lib/HomeBox.dart
@@ -10,7 +10,20 @@ class HomeBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(color: color, border: Border.all(width: 2), borderRadius: BorderRadius.circular(10)),
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(width: 2),
+        borderRadius: BorderRadius.circular(10),
+        //Shadow
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 5,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
       height: 150,
       child: Row(
         // crossAxisAlignment: CrossAxisAlignment.center,
